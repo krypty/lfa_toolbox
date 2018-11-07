81e3949b49bb1f105a55e6ddabce1b412ec5b772
Gary Marigliano <gmarigliano93@gmail.com>
2018-11-07 11:14:28 +0100

    Extract main() of triangular_mf.py into proper test

---
+++ tests/test_triangular_mf.py
@@ -0,0 +1,12 @@
+from lfa_toolbox.fs.core.mf.triangular_mf import TriangularMF
+
+
+def test_fuzzification():
+    mf = TriangularMF(p_min=2, p_mid=15, p_max=16, n_points=50)
+    assert mf.fuzzify(-2) == 0
+
+    # large margin because it is rounded (i.e. limited by the number of points)
+    assert abs(mf.fuzzify(2.5) - 0.03846) < 0.1
+
+    assert mf.fuzzify(15) == 1
+    assert mf.fuzzify(118) == 0
