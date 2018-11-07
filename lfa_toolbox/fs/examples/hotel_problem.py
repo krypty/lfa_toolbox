163e6c9e636ddc32d1db4158bcec5b330c6a7e22
Gary Marigliano <gmarigliano93@gmail.com>
2018-11-07 11:20:21 +0100

    Reformat examples/

--- lfa_toolbox/fs/examples/hotel_problem.py
+++ lfa_toolbox/fs/examples/hotel_problem.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 from lfa_toolbox.fs.core.fis.fis import FIS, OR_max, COA_func, MIN
-from lfa_toolbox.fs.core.lv.linguistic_variable import \
-    LinguisticVariable
-from lfa_toolbox.fs.core.mf.lin_piece_wise_mf import \
-    LinPWMF
+from lfa_toolbox.fs.core.lv.linguistic_variable import LinguisticVariable
+from lfa_toolbox.fs.core.mf.lin_piece_wise_mf import LinPWMF
 from lfa_toolbox.fs.core.rules.fuzzy_rule import FuzzyRule
-from lfa_toolbox.fs.core.rules.fuzzy_rule_element import Consequent, \
-    Antecedent
+from lfa_toolbox.fs.core.rules.fuzzy_rule_element import Consequent, Antecedent
 from lfa_toolbox.fs.view.fis_viewer import FISViewer
 
 
 def hotel_problem():
-    lv_service = LinguisticVariable(name="service", ling_values_dict={
-        "bad": LinPWMF([0, 1], [4, 0]),
-        "average": LinPWMF([4, 0], [5, 1], [6, 0]),
-        "good": LinPWMF([6, 0], [7, 1], [8, 0]),
-        "very good": LinPWMF([7, 0], [9, 1], [10, 1])
-    })
+    lv_service = LinguisticVariable(
+        name="service",
+        ling_values_dict={
+            "bad": LinPWMF([0, 1], [4, 0]),
+            "average": LinPWMF([4, 0], [5, 1], [6, 0]),
+            "good": LinPWMF([6, 0], [7, 1], [8, 0]),
+            "very good": LinPWMF([7, 0], [9, 1], [10, 1]),
+        },
+    )
 
     # LinguisticVariableViewer(lv_service).show()
 
@@ -25,30 +25,22 @@ def hotel_problem():
         name="recommendation",
         ling_values_dict={
             "low": LinPWMF([0, 0], [100, 1]),
-            "high": LinPWMF([0, 1], [100, 0])
-        })
+            "high": LinPWMF([0, 1], [100, 0]),
+        },
+    )
 
     r1 = FuzzyRule(
-        ants=[
-            Antecedent(lv_service, "good"),
-            Antecedent(lv_service, "very good")
-        ],
+        ants=[Antecedent(lv_service, "good"), Antecedent(lv_service, "very good")],
         ant_act_func=OR_max,
-        cons=[
-            Consequent(lv_recommendation, "high")
-        ],
-        impl_func=MIN
+        cons=[Consequent(lv_recommendation, "high")],
+        impl_func=MIN,
     )
 
     print(r1)
 
-    fis = FIS(
-        aggr_func=np.max,
-        defuzz_func=COA_func,
-        rules=[r1]
-    )
+    fis = FIS(aggr_func=np.max, defuzz_func=COA_func, rules=[r1])
 
-    input_values = {'service': 7.5}
+    input_values = {"service": 7.5}
     predicted_value = fis.predict(input_values)["recommendation"]
 
     FISViewer(fis).show()
@@ -62,7 +54,7 @@ def hotel_problem():
     print("difference     : {}".format(expected_value - predicted_value))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # import cProfile
     # cProfile.run("[resort_problem() for _ in range(1000)]")
     hotel_problem()
